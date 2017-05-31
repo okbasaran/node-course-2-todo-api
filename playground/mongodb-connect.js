6f9555a3e556ae271f638be2fe1ab6cd80cf5da9
Oytun Basaran <oytunkbasaran@gmail.com>
2017-05-31 18:04:32 +0300

    add delete script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,29 +6,29 @@ if (err) {
 }    
     console.log('Connected to MongoDB server');
     
-//    db.collection('Todos').insertOne({
-//        text: 'Something to do',
-//        completed: false
-//    }, (err,result) => {
-//        if (err) {
-//            return console.log('Unable to insert todo', err);
-//        }
-//        
-//        console.log(JSON.stringify(result.ops,undefined, 2));
-//        
-//    });
-    
-    db.collection('Users').insertOne({
-     name:'Oytun',
-     age: 31,
-     location: 'World'
-    },(err, result)=> {
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    }, (err,result) => {
         if (err) {
-            return console.log('Unable to insert data');
+            return console.log('Unable to insert todo', err);
         }
-        console.log(JSON.stringify(result.ops[0]._id.getTimestamp(),undefined, 2))
         
-    })
+        console.log(JSON.stringify(result.ops,undefined, 2));
+        
+    });
+    
+//    db.collection('Users').insertOne({
+//     name:'Oytun',
+//     age: 31,
+//     location: 'World'
+//    },(err, result)=> {
+//        if (err) {
+//            return console.log('Unable to insert data');
+//        }
+//        console.log(JSON.stringify(result.ops[0]._id.getTimestamp(),undefined, 2))
+//        
+//    })
     
     db.close();
 });
